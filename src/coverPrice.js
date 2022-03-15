958e5bca62e159605a8eadb28f395c3527eceb3d
danoctavian <danoctavian91@gmail.com>
2022-03-15 20:46:25 +0200

    fix price computation

--- src/coverPrice.js
+++ src/coverPrice.js
@@ -1,38 +1,41 @@
-const { BigNumber } = require('ethers');
+const { BigNumber,  utils: { parseUnits } } = require('ethers');
 
-const SURGE_THRESHOLD = BigNumber.from(8e17.toString());
-const BASE_SURGE_LOADING = BigNumber.from(1e17.toString());
 
-const PRICE_RATIO_CHANGE_PER_DAY = BigNumber.from(100);
-const PRICE_DENOMINATOR = BigNumber.from(10000)
-const BASE_PRICE_BUMP_RATIO = BigNumber.from(200); // 2%
-const BASE_PRICE_BUMP_INTERVAL = BigNumber.from(1000); // 10%
-const BASE_PRICE_BUMP_DENOMINATOR = BigNumber.from(10000);
+const SURGE_THRESHOLD = parseUnits('0.8');
+const BASE_SURGE_LOADING = parseUnits('0.1'); // 10%
+const BASE_SURGE_CAPACITY_USED = parseUnits('0.01'); // 1%
+
+const PRICE_RATIO_CHANGE_PER_DAY = parseUnits('0.005'); // 0.5%
+const BASE_PRICE_BUMP_RATIO = parseUnits('0.02'); // 2%
+const BASE_PRICE_BUMP_INTERVAL = 1000; // 10%
+const BASE_PRICE_BUMP_DENOMINATOR = 10000;
+
+const PRICE_DENOMINATOR = parseUnits('1');
 
 function interpolatePrice (
-  lastPrice,
-  targetPrice,
+  lastPriceRatio,
+  targetPriceRatio,
   lastPriceUpdate,
   currentTimestamp,
 ) {
 
-  const priceChange = (currentTimestamp - lastPriceUpdate) / (24 * 3600) * PRICE_RATIO_CHANGE_PER_DAY;
+  const priceChange = BigNumber.from(currentTimestamp - lastPriceUpdate).div(24 * 3600).mul(PRICE_RATIO_CHANGE_PER_DAY);
 
-  if (targetPrice > lastPrice) {
-    return targetPrice;
+  if (lastPriceRatio.lt(targetPriceRatio.add(priceChange))) {
+    return targetPriceRatio;
   }
 
-  return lastPrice.sub(lastPrice.sub(targetPrice).mul(priceChange).div(PRICE_DENOMINATOR));
+  return lastPriceRatio.sub(priceChange);
 }
 
 function calculatePrice (
   amount,
-  basePrice,
+  basePriceRatio,
   activeCover,
   capacity) {
 
   amount = BigNumber.from(amount);
-  basePrice = BigNumber.from(basePrice);
+  basePriceRatio = BigNumber.from(basePriceRatio);
   activeCover = BigNumber.from(activeCover);
   capacity = BigNumber.from(capacity);
 
@@ -41,15 +44,22 @@ function calculatePrice (
   const newActiveCoverRatio = newActiveCoverAmount.mul(1e18.toString()).div(capacity);
 
   if (newActiveCoverRatio.lt(SURGE_THRESHOLD)) {
-    return basePrice;
+    return basePriceRatio;
   }
 
-  const surgeLoadingRatio = newActiveCoverRatio.sub(SURGE_THRESHOLD);
-  const surgeFraction =
-    activeCoverRatio.gte(SURGE_THRESHOLD) ? BigNumber.from(1e18.toString()) : surgeLoadingRatio.mul(capacity).div(amount);
-  const surgeLoading = surgeLoadingRatio.mul(BASE_SURGE_LOADING).div(1e16.toString()).div(2).mul(surgeFraction).div(1e18.toString());
+  const capacityUsedSteepRatio = activeCoverRatio.gte(SURGE_THRESHOLD) ? newActiveCoverRatio.sub(activeCoverRatio) : newActiveCoverRatio.sub(SURGE_THRESHOLD);
+  const capacityUsedRatio = newActiveCoverRatio.sub(activeCoverRatio);
 
-  return basePrice.mul(surgeLoading.add(1e18.toString())).div(1e18.toString());
+  const startSurgeLoadingRatio =
+    activeCoverRatio.lt(SURGE_THRESHOLD) ? BigNumber.from(0)
+      : activeCoverRatio.sub(SURGE_THRESHOLD).mul(BASE_SURGE_LOADING).div(BASE_SURGE_CAPACITY_USED);
+  const endSurgeLoadingRatio = newActiveCoverRatio.sub(SURGE_THRESHOLD).mul(BASE_SURGE_LOADING).div(BASE_SURGE_CAPACITY_USED);
+
+  const surgeLoadingRatio = capacityUsedSteepRatio.mul(endSurgeLoadingRatio.add(startSurgeLoadingRatio).div(2)).div(capacityUsedRatio);
+
+  const actualPriceRatio = basePriceRatio.mul(surgeLoadingRatio.add(PRICE_DENOMINATOR)).div(PRICE_DENOMINATOR);
+  return actualPriceRatio;
+
 }
 
 function getPrices (
@@ -70,33 +80,21 @@ function getPrices (
   const lastBasePriceValue = BigNumber.from(lastBasePrice.value);
   const lastUpdateTime = BigNumber.from(lastBasePrice.lastUpdateTime);
 
-  // limit amount to what is left of the total capacity
-  const capacityLeft = capacity.sub(activeCover);
-  if (capacityLeft.lt(amount)) {
-    amount = capacityLeft;
-  }
-
   const basePrice = interpolatePrice(
     lastBasePriceValue.gt(0) ? lastBasePriceValue : initialPrice,
     targetPrice,
     lastUpdateTime,
     blockTimestamp,
   );
-
-  console.log({
-    lastBasePriceValue: lastBasePriceValue.toString(),
-    basePrice: basePrice.toString(),
-  });
-
   // calculate actualPrice using the current basePrice
   const actualPrice = calculatePrice(amount, basePrice, activeCover, capacity);
 
   // Bump base price by 2% (200 basis points) per 10% (1000 basis points) of capacity used
-  const priceBump = amount.mul(BASE_PRICE_BUMP_DENOMINATOR).div(capacity).div(BASE_PRICE_BUMP_INTERVAL).mul(BASE_PRICE_BUMP_RATIO);
+  const priceBump = BASE_PRICE_BUMP_RATIO.mul(amount).mul(BASE_PRICE_BUMP_DENOMINATOR).div(capacity).div(BASE_PRICE_BUMP_INTERVAL);
 
   const bumpedBasePrice = basePrice.add(priceBump);
 
-  return { basePrice: bumpedBasePrice, actualPrice, coveredAmount: amount };
+  return { basePrice: bumpedBasePrice, actualPrice };
 }
 
 
--- test/unit/coverPrice.js
+++ test/unit/coverPrice.js
@@ -1,18 +1,40 @@
 const ethers = require('ethers');
-const assert = require('assert')
-const { parseEther } = ethers.utils;
+const assert = require('assert');
+const { BigNumber } = ethers;
+const { parseEther, parseUnits } = ethers.utils;
 const { getPrices } = require('../../src/coverPrice');
 
-describe('cover price', function () {
-  describe('getPrices()', function () {
-    const amount = parseEther('1000');
+function bnEqual (actual, expected, message) {
 
-    const activeCover = parseEther('8000');
-    const capacity = parseEther('10000');
-    const initialPrice = '1000';
-    const lastBasePrice = { value: '300', lastUpdateTime: 0 };
-    const targetPrice = '150';
-    const blockTimestamp = 24 * 3600;
+  const actualBN = BigNumber.from(actual);
+  const expectedBN = BigNumber.from(expected);
+  const error = message || `expected ${actualBN.toString()} to equal ${expectedBN.toString()}`;
+
+  if (actualBN.eq(expectedBN)) {
+    return;
+  }
+
+  throw new assert.AssertionError({
+    message: error,
+    actual: actualBN.toString(),
+    expected: expectedBN.toString(),
+    operator: 'bnEqual',
+  });
+}
+
+
+describe('getPrices', function () {
+
+  it('should calculate price correctly for active cover starting at 0 without surge', async function () {
+    const { stakingPool } = this;
+
+    const amount = parseUnits('2400');
+    const activeCover = parseUnits('0');
+    const capacity = parseUnits('50000');
+    const initialPrice = parseUnits('0.2');
+    const lastBasePrice = { value: parseUnits('0.02'), lastUpdateTime: 0 };
+    const targetPrice = parseUnits('0.02');
+    const blockTimestamp = 24 * 3600 * 183;
 
     const { actualPrice, basePrice: newBasePrice } = getPrices(
       amount,
@@ -24,10 +46,151 @@ describe('cover price', function () {
       blockTimestamp,
     );
 
-     const expectedNewBasePrice = '499';
-    const expectedActualPrice = '448';
+    const expectedNewBasePrice = parseUnits('0.0296');
+    const expectedActualPrice = parseUnits('0.02');
 
-    assert.equal(newBasePrice.toString(), expectedNewBasePrice.toString());
-    assert.equal(actualPrice.toString(), expectedActualPrice.toString());
+    bnEqual(newBasePrice, expectedNewBasePrice);
+    bnEqual(actualPrice, expectedActualPrice);
   });
+
+  it('should calculate price correctly for active cover without surge starting off at base price = target price', async function () {
+    const { stakingPool } = this;
+
+    const amount = parseUnits('12000');
+    const activeCover = parseUnits('2400');
+    const capacity = parseUnits('50000');
+    const initialPrice = parseUnits('0.2');
+    const lastBasePrice = { value: parseUnits('0.0296'), lastUpdateTime: 0 };
+    const targetPrice = parseUnits('0.02');
+    const blockTimestamp = 24 * 3600 * 3;
+
+    const { actualPrice, basePrice: newBasePrice } = getPrices(
+      amount,
+      activeCover,
+      capacity,
+      initialPrice,
+      lastBasePrice,
+      targetPrice,
+      blockTimestamp,
+    );
+
+    const expectedNewBasePrice = parseUnits('0.068');
+    const expectedActualPrice = parseUnits('0.02');
+
+    bnEqual(newBasePrice, expectedNewBasePrice);
+    bnEqual(actualPrice, expectedActualPrice);
+  });
+
+  it('should calculate price correctly for active cover without surge starting off at base price > target price', async function () {
+    const { stakingPool } = this;
+
+    const amount = parseUnits('12000');
+    const activeCover = parseUnits('14400');
+    const capacity = parseUnits('50000');
+    const initialPrice = parseUnits('0.2');
+    const lastBasePrice = { value: parseUnits('0.068'), lastUpdateTime: 0 };
+    const targetPrice = parseUnits('0.02');
+    const blockTimestamp = 24 * 3600 * 5;
+
+    const { actualPrice, basePrice: newBasePrice } = getPrices(
+      amount,
+      activeCover,
+      capacity,
+      initialPrice,
+      lastBasePrice,
+      targetPrice,
+      blockTimestamp,
+    );
+
+    const expectedNewBasePrice = parseUnits('0.091');
+    const expectedActualPrice = parseUnits('0.043');
+
+    bnEqual(newBasePrice, expectedNewBasePrice);
+    bnEqual(actualPrice, expectedActualPrice);
+  });
+
+  it('should calculate price correctly for active cover without surge which increases base price', async function () {
+    const { stakingPool } = this;
+
+    const amount = parseUnits('12000');
+    const activeCover = parseUnits('26400');
+    const capacity = parseUnits('50000');
+    const initialPrice = parseUnits('0.2');
+    const lastBasePrice = { value: parseUnits('0.091'), lastUpdateTime: 0 };
+    const targetPrice = parseUnits('0.02');
+    const blockTimestamp = 24 * 3600 * 5;
+
+    const { actualPrice, basePrice: newBasePrice } = getPrices(
+      amount,
+      activeCover,
+      capacity,
+      initialPrice,
+      lastBasePrice,
+      targetPrice,
+      blockTimestamp,
+    );
+
+    const expectedNewBasePrice = parseUnits('0.114');
+    const expectedActualPrice = parseUnits('0.066');
+
+    bnEqual(newBasePrice, expectedNewBasePrice);
+    bnEqual(actualPrice, expectedActualPrice);
+  });
+
+  it('should calculate price correctly for active cover with both flat and surge pricing', async function () {
+    const { stakingPool } = this;
+
+    const amount = parseUnits('8000');
+    const activeCover = parseUnits('38400');
+    const capacity = parseUnits('50000');
+    const initialPrice = parseUnits('0.2');
+    const lastBasePrice = { value: parseUnits('0.114'), lastUpdateTime: 0 };
+    const targetPrice = parseUnits('0.02');
+    const blockTimestamp = 24 * 3600 * 15;
+
+    const { actualPrice, basePrice: newBasePrice } = getPrices(
+      amount,
+      activeCover,
+      capacity,
+      initialPrice,
+      lastBasePrice,
+      targetPrice,
+      blockTimestamp,
+    );
+
+    const expectedNewBasePrice = parseUnits('0.071');
+    const expectedActualPrice = parseUnits('0.058968');
+
+    bnEqual(newBasePrice, expectedNewBasePrice);
+    bnEqual(actualPrice, expectedActualPrice);
+  });
+
+  it('should calculate price correctly for active cover with surge pricing only', async function () {
+    const { stakingPool } = this;
+
+    const amount = parseEther('2400');
+    const activeCover = parseEther('46400');
+    const capacity = parseEther('50000');
+    const initialPrice = parseUnits('0.2');
+    const lastBasePrice = { value: parseUnits('0.071'), lastUpdateTime: 0 };
+    const targetPrice = parseUnits('0.02');
+    const blockTimestamp = 24 * 3600 * 10;
+
+    const { actualPrice, basePrice: newBasePrice } = getPrices(
+      amount,
+      activeCover,
+      capacity,
+      initialPrice,
+      lastBasePrice,
+      targetPrice,
+      blockTimestamp,
+    );
+
+    const expectedNewBasePrice = parseUnits('0.0306');
+    const expectedActualPrice = parseUnits('0.05292');
+
+    bnEqual(newBasePrice, expectedNewBasePrice);
+    bnEqual(actualPrice, expectedActualPrice);
+  });
+
 });
