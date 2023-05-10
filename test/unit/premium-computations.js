7d49d2b361636ed9ef80e166a851481c6bca4371
Dan Octavian <dan@MacBook-Pro.local>
2023-05-11 00:37:21 +0300

    add unit test

---
+++ test/unit/premium-computations.js
@@ -0,0 +1,50 @@
+const sinon = require('sinon');
+const {
+  utils: { parseEther },
+} = require('ethers');
+const { expect } = require('chai');
+const mockStore = require('../mocks/store');
+const { BigNumber } = require('ethers');
+const { MIN_COVER_PERIOD } = require('../../src/lib/constants');
+const { calculateOptimalPoolAllocation } = require('../../src/lib/premium-computations');
+
+describe.only('Premium computations tests', () => {
+  const store = { getState: () => null };
+
+  afterEach(function() {
+    sinon.restore();
+  });
+
+
+  it('returns optimal pool allocation', () => {
+
+    const pool1 = {
+      basePrice: BigNumber.from('300'),
+      initialCapacityUsed: parseEther('1000'),
+      totalCapacity: parseEther('10000')
+    };
+
+    const pool2 = {
+      basePrice: BigNumber.from('200'),
+      initialCapacityUsed: parseEther('1000'),
+      totalCapacity: parseEther('10000')
+    };
+
+    const pool3 = {
+      basePrice: BigNumber.from('400'),
+      initialCapacityUsed: parseEther('1000'),
+      totalCapacity: parseEther('10000')
+    };
+
+    let i = 0;
+    const pools = [pool1, pool2, pool3];
+    pools.forEach(pool =>{
+      pool.poolId = i++;
+    });
+
+    const amount = parseEther('1000');
+    const optimalAllocation = calculateOptimalPoolAllocation(amount, pools);
+
+    console.log(optimalAllocation);
+  });
+});
