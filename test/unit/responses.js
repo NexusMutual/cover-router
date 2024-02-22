44264db3727858e578225be686d9f588eb62f039
shark0der <commits@shark0der.com>
2024-02-22 13:51:40 +0200

    Fix tests failing due to removed annualPrice

--- test/unit/responses.js
+++ test/unit/responses.js
@@ -2,7 +2,6 @@ const capacities = [
   {
     productId: 0,
     allocatedNxm: '364800000000000000000',
-    annualPrice: '0.02',
     maxAnnualPrice: '0.03',
     minAnnualPrice: '0.02',
     availableCapacity: [
@@ -23,7 +22,6 @@ const capacities = [
   {
     productId: 1,
     allocatedNxm: '0',
-    annualPrice: '0.02',
     maxAnnualPrice: '0.03',
     minAnnualPrice: '0.02',
     availableCapacity: [
@@ -44,7 +42,6 @@ const capacities = [
   {
     productId: 2,
     allocatedNxm: '0',
-    annualPrice: '0.02',
     maxAnnualPrice: '0.02',
     minAnnualPrice: '0.02',
     availableCapacity: [
