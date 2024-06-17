2bb099ac1f4bcb97a0c9501f3296e6d05c5e7ab5
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-06-17 12:29:20 +0300

    Add quoteEngine custom pool priority test

--- test/unit/quoteEngine.js
+++ test/unit/quoteEngine.js
@@ -10,14 +10,13 @@ const { quoteEngine } = require('../../src/lib/quoteEngine');
 const mockStore = require('../mocks/store');
 
 describe('Quote Engine tests', () => {
-  const store = { getState: () => null };
+  const store = { getState: () => mockStore };
 
   afterEach(function () {
     sinon.restore();
   });
 
   it('should return quote in ETH for product 1 for 1 ETH for minimal cover period', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
     const productId = 1;
     const amount = parseEther('1');
 
@@ -31,7 +30,6 @@ describe('Quote Engine tests', () => {
   });
 
   it('should return quote in DAI for product 1 for minimal cover period', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
     const productId = 1;
     const amount = parseEther('1000');
 
@@ -45,7 +43,6 @@ describe('Quote Engine tests', () => {
   });
 
   it('should return quote in USDC for product 1 for minimal cover period', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
     const productId = 1;
     const amount = parseUnits('1000', 6);
 
@@ -59,7 +56,6 @@ describe('Quote Engine tests', () => {
   });
 
   it('should return quote with split across 2 pools', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
     const productId = 1;
     const amount = parseEther('12000');
 
@@ -89,7 +85,6 @@ describe('Quote Engine tests', () => {
   });
 
   it('should return quote for a product with fixed price', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
     const productId = 2;
     const amount = parseEther('1');
 
@@ -102,8 +97,32 @@ describe('Quote Engine tests', () => {
     expect(quote.coverAmountInAsset.toString()).to.be.equal('1000042320824328192');
   });
 
+  it('should return return allocation in order of custom pool priority for productId 4', () => {
+    const productId = 4;
+    const amount = parseEther('102000');
+
+    const [quote1, quote2, quote3] = quoteEngine(store, productId, amount, MIN_COVER_PERIOD, 1);
+
+    expect(quote1.poolId).to.be.equal(1); // partially filled
+    expect(quote1.premiumInNxm.toString()).to.be.equal('46158904109589041');
+    expect(quote1.premiumInAsset.toString()).to.be.equal('1325888626040652254');
+    expect(quote1.coverAmountInNxm.toString()).to.be.equal('28080000000000000000');
+    expect(quote1.coverAmountInAsset.toString()).to.be.equal('806582247508063456204');
+
+    expect(quote2.poolId).to.be.equal(18); // capacity filled
+    expect(quote2.premiumInNxm.toString()).to.be.equal('10973457534246575342');
+    expect(quote2.premiumInAsset.toString()).to.be.equal('315206411713208509713');
+    expect(quote2.coverAmountInNxm.toString()).to.be.equal('1668880000000000000000');
+    expect(quote2.coverAmountInAsset.toString()).to.be.equal('47937641781383794187687');
+
+    expect(quote3.poolId).to.be.equal(22); // capacity filled
+    expect(quote3.premiumInNxm.toString()).to.be.equal('11809917123287671232');
+    expect(quote3.premiumInAsset.toString()).to.be.equal('339233244166144344759');
+    expect(quote3.coverAmountInNxm.toString()).to.be.equal('1854030000000000000000');
+    expect(quote3.coverAmountInAsset.toString()).to.be.equal('53255971664792553052225');
+  });
+
   it('should return empty array for cover over the capacity', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
     const productId = 1;
     const amount = parseEther('300000');
 
@@ -113,8 +132,7 @@ describe('Quote Engine tests', () => {
   });
 
   it('should return null non existing product', () => {
-    sinon.stub(store, 'getState').callsFake(() => mockStore);
-    const productId = 4;
+    const productId = 999;
     const amount = BigNumber.from(30);
 
     const quote = quoteEngine(store, productId, amount, MIN_COVER_PERIOD, 1);
